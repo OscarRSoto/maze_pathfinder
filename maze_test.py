0a968c2c671ea10f3f8e0f6662bbe073b22eac95
OscarRSoto <64520377+OscarRSoto@users.noreply.github.com>
2021-02-12 02:35:33 -0700

    Update maze_test.py

--- maze_test.py
+++ maze_test.py
@@ -1,14 +1,15 @@
 import maze
 import pytest
 
-@pytest.mark.parametrize('maze_name, objetivo',
+@pytest.mark.parametrize('archivo, objetivo',
+
                          [('maze1.txt',(0, 5)),
                           ('maze2.txt',(8, 13)),
                           ('maze3.txt',(2, 1)),
                           ('maze4.txt',(1, 1)),
                           ('maze5.txt',(18, 25))
                           ])
-def test_maze(maze_name, objetivo):
-    m = maze.Maze(maze_name)
+def test_maze(archivo, objetivo):
+    m = maze.Maze(archivo)
     m.solve
-    assert m.goal == objetivo
+   assert m.goal==objetivo "sample text"
